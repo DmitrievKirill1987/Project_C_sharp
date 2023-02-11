92cb8ab9fb0c40c1f0296d7fe65b0db0a48df21c
Kirill <frantic.87@mail.ru>
2023-02-11 15:23:58 +0300

    added Task03 switch case

--- Seminar01/Task03/Program.cs
+++ Seminar01/Task03/Program.cs
@@ -1,37 +1,84 @@
 ﻿//Программа переводит числа в названия дней недели
 
+// Console.Write("Введите номер дня недели: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (day == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (day == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (day == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (day == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (day == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (day == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else if (day > 7)
+// {
+//     Console.WriteLine("Такого дня недели не существует!!!");
+// }
+
 Console.Write("Введите номер дня недели: ");
 int day = Convert.ToInt32(Console.ReadLine());
 
-if(day == 1)
+switch (day)
 {
-    Console.WriteLine("Понедельник");
-}
-else if(day == 2)
-{
-    Console.WriteLine("Вторник");
-}
-else if(day == 3)
-{
-    Console.WriteLine("Среда");
-}
-else if(day == 4)
-{
-    Console.WriteLine("Четверг");
-}
-else if(day == 5)
-{
-    Console.WriteLine("Пятница");
-}
-else if(day == 6)
-{
-    Console.WriteLine("Суббота");
-}
-else if(day == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
-else if(day > 7)
-{
-    Console.WriteLine("Такого дня недели не существует!!!");
+    case 1:
+        {
+            Console.WriteLine("Понедельник");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("Вторник");
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine("Среда");
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine("Четверг");
+            break;
+        }
+    case 5:
+        {
+            Console.WriteLine("Пятница");
+            break;
+        }
+    case 6:
+        {
+            Console.WriteLine("Суббота");
+            break;
+        }
+    case 7:
+        {
+            Console.WriteLine("Воскресенье");
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("Такого дня недели не существует");
+            break;
+        }
 }
