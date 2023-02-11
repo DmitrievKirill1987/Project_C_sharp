7ed1f390f3ab45ed3d0611e7b5198ef9713120dc
Kirill <frantic.87@mail.ru>
2023-02-11 14:51:26 +0300

    added Demo programm

---
+++ Seminar01/Demo/Program.cs
@@ -0,0 +1,6 @@
+﻿//Программа принимает на вход число и выдает его квадрат
+
+Console.Write("Напишите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа равен: {square}");
