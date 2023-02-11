a0e878ca5e0a804421ade9bcdaa794d0cf7b64b9
Kirill <frantic.87@mail.ru>
2023-02-11 15:41:44 +0300

    added Task02

---
+++ Seminar01/Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿//Программа принимает число Н и показывает все целые цисла от -Н до Н
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+// int M = -N;
+
+// Console.Write("Все целые числа: ");
+// while(M <= N)
+// {
+//     Console.Write($"{M}, ");
+//     M ++;
+// }
+// Console.WriteLine();
+
+Console.Write("Все целые числа: ");
+for(int i = -N; i <= N; i++)
+{
+    Console.Write($"{i}, ");
+}
+Console.WriteLine();
