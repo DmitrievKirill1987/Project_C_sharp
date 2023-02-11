2fe3a4b7d48f5087e3ee3756d655561a34048b65
Kirill <frantic.87@mail.ru>
2023-02-11 19:26:50 +0300

    added Task 4 to dir HomeWorkSem01

---
+++ HomeWorkSem01/Task04/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти наибольшее из трех введенных чисел
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if ((a == b) && (b == c))
+{
+    Console.WriteLine("Числа между собой равны");
+}
+ if (max < b)
+{
+   max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.WriteLine($"Число {max} наибольшее");
