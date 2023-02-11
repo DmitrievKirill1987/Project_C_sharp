85fa564e20b87746889904d7b1ec28134efce636
Kirill <frantic.87@mail.ru>
2023-02-11 19:37:06 +0300

    added Task 6 to dir HomeWorkSem01

---
+++ HomeWorkSem01/Task06/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает число и выдает является ли число четным
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 0)
+{
+    Console.WriteLine("Число является нулем");
+}
+else
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine("Число является четным");
+    }
+    else
+    {
+        Console.WriteLine("Число является нечетным");
+    }
+}
