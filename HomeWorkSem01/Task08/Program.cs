8b569687493a5d9490177541af68d07c5961a682
Kirill <frantic.87@mail.ru>
2023-02-11 19:56:02 +0300

    added Task 8 to dir HomeWorkSem01

---
+++ HomeWorkSem01/Task08/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа принимает число Р и показывает все четные числа от 1 до Н
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 0)
+{
+    Console.Write($"Все четные числа от {N} до 1: ");
+    for (int i = N; i <= 1; i++)
+    {
+        if ((i % 2 == 0) && (i != 0))
+        {
+            Console.Write($"{i}, ");
+        }
+    };
+}
+else if (N > 0)
+{
+    Console.Write($"Все четные числа от 1 до {N}: ");
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($"{i}, ");
+        }
+    }
+}
+else
+{
+    Console.Write("Число является нулем");
+}
+Console.WriteLine();
