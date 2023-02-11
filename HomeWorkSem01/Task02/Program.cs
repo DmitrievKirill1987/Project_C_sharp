885045d282a393bf7045115edb0806369a80c688
Kirill <frantic.87@mail.ru>
2023-02-11 18:57:45 +0300

    added Task 2 to dir HomeWorkSem01

---
+++ HomeWorkSem01/Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа принимает два числа и выдает какое число больше, а какое меньше
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+{
+    Console.WriteLine($"Число {b} больше чем {a}");
+}
+else if (a > b)
+{
+    Console.WriteLine($"Число {a} больше чем {b}");
+}
+else
+{
+    Console.WriteLine($"Число {a} равно числу {b}");
+}
