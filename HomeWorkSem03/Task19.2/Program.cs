b1713166782b85b2f3fdd49d73776733ead2b108
Kirill <frantic.87@mail.ru>
2023-02-18 20:02:38 +0300

    added Task 19.2

---
+++ HomeWorkSem03/Task19.2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Проверка на палиндромность с применением строчного типа данных
+
+Console.Write("Введите число: ");
+string? N = Console.ReadLine();
+char[] arrN = N.ToCharArray();
+bool result = false;
+ 
+for(int i = 0; i <= (arrN.Length / 2); i++)
+{
+    if(arrN[i] == arrN[arrN.Length - 1 - i])
+    {
+        result = true;
+    }
+    else
+    {
+        break;
+    }
+}
+ 
+if(result == true)
+{
+    Console.WriteLine("Число является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом.");
+}
