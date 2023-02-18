4e7edd192589a0a748ad4316ddc89604c8cb333f
Kirill <frantic.87@mail.ru>
2023-02-18 19:35:23 +0300

    added Task 23

---
+++ HomeWorkSem03/Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов от 1 до N.
+
+Console.Write("Введите число: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if(N > 0)
+{
+    for(int i = 1; i <= N; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+}
+else
+{
+    Console.Write("Введено неверное значение !)");
+}
+Console.WriteLine();
