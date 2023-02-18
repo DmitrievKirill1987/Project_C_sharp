3c0b3d1e12aa2286a8a02f80c265521e3c15ddad
Kirill <frantic.87@mail.ru>
2023-02-18 15:08:40 +0300

    added Task 21

--- Seminar03/Task18/Program.cs
+++ Seminar03/Task18/Program.cs
@@ -2,24 +2,33 @@
 
 Console.Write("Введите номер четверти: ");
 int quarter = Convert.ToInt32(Console.ReadLine());
+string[] strArray = {"X > 0 Y > 0", "X < 0 Y > 0", "X < 0 Y < 0", "X > 0 Y < 0", "Такой четверти нет"};
+// if (quarter == 1)
+// {
+//     Console.WriteLine(strArr[0]);
+// }
+// else if (quarter == 2)
+// {
+//     Console.WriteLine(strArr[1]);
+// }
+// else if (quarter == 3)
+// {
+//     Console.WriteLine(strArr[2]);
+// }
+// else if (quarter == 4)
+// {
+//     Console.WriteLine(strArr[3]);
+// }
+// else
+// {
+//     Console.WriteLine(strArr[4]);
+// }
 
-if(quarter == 1)
+if(quarter > 0 && quarter < 5)
 {
-    Console.WriteLine("X > 0 Y > 0");
-}
-else if (quarter == 2)
-{
-    Console.WriteLine("X < 0 Y > 0");
-}
-else if (quarter == 3)
-{
-    Console.WriteLine("X < 0 Y < 0");
-}
-else if (quarter == 4)
-{
-    Console.WriteLine("X > 0 Y < 0");
+    Console.WriteLine(strArray[quarter-1]);
 }
 else
 {
-    Console.WriteLine("Четверть не выбрана");
+    Console.WriteLine(strArray[4]);
 }
---
+++ Seminar03/Task21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа принимает на вход координаты двух точек и находит расстояние между ними в 2д пространстве
+
+int[] coords1 = new int[2];
+Console.Write("Введите Х первой точки: ");
+coords1[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y первой точки: ");
+coords1[1] = Convert.ToInt32(Console.ReadLine());
+
+int[] coords2 = new int[2];
+Console.Write("Введите Х второй точки: ");
+coords2[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y второй точки: ");
+coords2[1] = Convert.ToInt32(Console.ReadLine());
+
+double dis = Math.Round(Math.Sqrt(Math.Pow((coords1[0] - coords2[0]), 2) + Math.Pow((coords1[1] - coords2[1]), 2)), 2);
+
+Console.WriteLine($"Расстояние между точками: {dis}");
