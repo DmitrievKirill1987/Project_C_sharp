62129ce3e4a37de5215a0645c571f2f80621ce98
Kirill <frantic.87@mail.ru>
2023-02-18 21:55:11 +0300

    added Task001

---
+++ AdditionalHomeWork/Task001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу по нахождению факториала числа N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int A = 1;
+if (N < 0)
+{
+    Console.WriteLine("Факториала отрицательного числа не существует");
+}
+else if (N == 0)
+{
+    Console.WriteLine("Факториал нуля равен 1");
+}
+else
+{
+    while (i <= N)
+    {
+        A = A * i; 
+        i++;
+    }
+    Console.WriteLine($"Факториал числа N равен {A} ");
+}
