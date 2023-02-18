261f0e03dce562778996c3b08abd427ddbc63d1b
Kirill <frantic.87@mail.ru>
2023-02-18 14:38:45 +0300

    added Task 17 and Task 18

---
+++ Seminar03/Task17/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа принимает на вход координаты точки X и Y (не равны нулю) и выдает номер четверти плоскости 
+//в которой эта точка находится
+
+int[] coords = new int[2];
+
+Console.Write("Введите Х: ");
+coords[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: ");
+coords[1] = Convert.ToInt32(Console.ReadLine());
+
+if(coords[0] > 0 && coords[1] > 0)
+{
+    Console.WriteLine("Мы находимся в первой четверти");
+}
+else if (coords[0] < 0 && coords[1] > 0)
+{
+    Console.WriteLine("Мы находимся во второй четверти");
+}
+else if (coords[0] < 0 && coords[1] < 0)
+{
+    Console.WriteLine("Мы находимся в третьей четверти");
+}
+else if (coords[0] > 0 && coords[1] < 0)
+{
+    Console.WriteLine("Мы находимся в четвертой четверти");
+}
+else
+{
+    Console.WriteLine("Координата находится на осях");
+}
---
+++ Seminar03/Task18/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти
+
+Console.Write("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if(quarter == 1)
+{
+    Console.WriteLine("X > 0 Y > 0");
+}
+else if (quarter == 2)
+{
+    Console.WriteLine("X < 0 Y > 0");
+}
+else if (quarter == 3)
+{
+    Console.WriteLine("X < 0 Y < 0");
+}
+else if (quarter == 4)
+{
+    Console.WriteLine("X > 0 Y < 0");
+}
+else
+{
+    Console.WriteLine("Четверть не выбрана");
+}
