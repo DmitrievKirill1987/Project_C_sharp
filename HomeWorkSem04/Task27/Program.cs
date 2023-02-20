4d32b8d5161731f42f0a90794167276ae2821bf3
Kirill <frantic.87@mail.ru>
2023-02-20 21:24:28 +0300

    added Task29

--- HomeWorkSem04/Task27/Program.cs
+++ HomeWorkSem04/Task27/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 int ReadInt(string text)
 {
-Console.Write(text);
-  return Convert.ToInt32(Console.ReadLine());
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
 int SymbolsCount(int N)
@@ -19,10 +19,11 @@ int SymbolsCount(int N)
     else i = 1;
     return i;
 }
+
 int number = ReadInt("Введите число: ");
 double s = 0;
-for (int i=1; i<=SymbolsCount(number); i++ )
+for (int i = 1; i <= SymbolsCount(number); i++)
 {
-  s = Math.Truncate(s + number % Math.Pow(10, i)/ Math.Pow(10, i-1));
+    s = Math.Truncate(s + number % Math.Pow(10, i) / Math.Pow(10, i - 1));
 }
 Console.WriteLine($"Сумма цифр в числе {s}.");
---
+++ HomeWorkSem04/Task29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа спрашивает длину массива, заполняет массив числами и выводит массив на экран
+
+int ArrLong(string text)
+{
+    Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+void GenArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random rand = new Random();
+        array[i] = rand.Next(0, 100);
+    }
+}
+
+void PrintArr(int[] array)
+{
+    Console.WriteLine("[" + string.Join(",", array) + "]");
+    Console.WriteLine();
+}
+
+int[] arr = new int[ArrLong("Введи длину массива: ")];
+GenArr(arr);
+PrintArr(arr);
