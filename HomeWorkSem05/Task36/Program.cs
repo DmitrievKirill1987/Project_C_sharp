173494505631c65abfdb80a9760194aef180259a
Kirill <frantic.87@mail.ru>
2023-02-26 14:14:29 +0300

    added Task 36

---
+++ HomeWorkSem05/Task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int ArrLong(string text)
+{
+    Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+void GenArr(int[] array, int leftRange, int rightRange)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random rand = new Random();
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+}
+
+void PrintArr(int[] array)
+{
+    Console.WriteLine("[" + string.Join(",", array) + "]");
+}
+
+int OddSum(int[] array)
+{
+  int sum = 0; 
+    for(int i = 1; i < array.Length; i +=2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] arr = new int[ArrLong("Введите длину массива: ")];
+GenArr(arr, -1000, 1000);
+PrintArr(arr);
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях {OddSum(arr)}");
