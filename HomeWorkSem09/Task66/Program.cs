bf42008e02dcd3fe59b907a2c3fc9b357e909ae0
Kirill <frantic.87@mail.ru>
2023-03-12 19:18:27 +0300

    added HomeWorkSem 09

---
+++ HomeWorkSem09/Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumber(int m, int n)
+{
+    if (n == m) return n;
+    return n + SumNumber(m, n - 1);
+}
+
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+if (M < N) Console.WriteLine($"Сумма чисел от {M} до {N}: {SumNumber(M, N)}");
+else Console.WriteLine($"Сумма чисел от {N} до {M}: {SumNumber(N, M)}");
---
+++ HomeWorkSem09/Task68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+if (M >= 0 && N >= 0) Console.WriteLine($"Функция Аккермана для чисел {M} и {N}: {Ackermann(M, N)}");
+else Console.WriteLine("Числа должны быть неотрицательные.");
